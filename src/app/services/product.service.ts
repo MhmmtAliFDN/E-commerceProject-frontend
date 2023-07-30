ccf7ceb783daf278a3fd54256443fff2bab1d53b
Muhammet Ali Fidan <mhmmtalifdn@gmail.com>
2023-07-30 23:22:45 +0300

    Added Base Components, Models and Services

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  apiUrl = 'https://localhost:44373/api/products/';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProducts(): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+    let newPath = this.apiUrl + "getbycategory?categoryId=" + categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath)
+  }
+}
